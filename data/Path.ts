118e21c6e8f53af250fb89a26de8a8bcb4dfa714
happy <happy>
2019-09-09 19:02:42 +0800

    通比牛牛预加载

--- data/Path.ts
+++ data/Path.ts
@@ -6,5 +6,6 @@ module gametbniuniu.data {
 		static readonly music_tbniuniu: string = 'music/tbniuniu/';
 		static readonly ui_tbniuniu: string = "tbniuniu_ui/game_ui/tbniuniu/";
 		static readonly atlas_game_ui: string = "tbniuniu_res/atlas/tbniuniu_ui/game_ui/";
+		static readonly ui_tbniuniu_sk: string = "tbniuniu_ui/game_ui/tbniuniu/sk";
 	}
 }
