931fc2f3b98459063cacf11e19dda3e6b76dbd19
xiaoer <xiaoer@example.com>
2019-10-18 19:53:50 +0800

    修改 充值按钮增加api判断

--- page/TbNiuNiuMapPage.ts
+++ page/TbNiuNiuMapPage.ts
@@ -91,6 +91,9 @@ module gametbniuniu.page {
         // 页面打开时执行函数
         protected onOpen(): void {
             super.onOpen();
+             //api充值不显示
+            this._viewUI.btn_chongzhi.visible = !WebConfig.enterGameLocked;
+            
             this._viewUI.btn_spread.on(LEvent.CLICK, this, this.onBtnClickWithTween);
             this._viewUI.btn_cardType.on(LEvent.CLICK, this, this.onBtnClickWithTween);
             this._viewUI.btn_back.on(LEvent.CLICK, this, this.onBtnClickWithTween);
