15dfca76598410fa24805e4341fca0b0977f6a55
xiaoer <xiaoer@example.com>
2019-10-19 12:52:34 +0800

    修改 金币不足弹窗增加api判断

--- page/TbNiuNiuPage.ts
+++ page/TbNiuNiuPage.ts
@@ -135,7 +135,7 @@ module gametbniuniu.page {
 			this._game.alert(StringU.substitute("老板，您的金币少于{0}哦~\n补充点金币去大杀四方吧~", limit), () => {
 				this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
 			}, () => {
-			}, true);
+			}, true,Tips.TIPS_SKIN_STR["cz"]);
 		}
 
 		public close(): void {
